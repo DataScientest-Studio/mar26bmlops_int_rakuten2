1b7bc9028cd6882afe27427cd043ce88acd9d4ef
Mirco <mirco.kappel@gmx.de>
2026-03-30 16:04:32 +0200

    update_cuda_fix_mlflow_working

--- src/pipeline.py
+++ src/pipeline.py
@@ -152,3 +152,8 @@ if __name__ == "__main__":
 
 # Space mk: Dev
 # cp /home/mirco/rakuten2/db/rakuten_colors.db /mnt/c/02_Project_MLOPS/ 
+# mlflow server \
+# --backend-store-uri mlruns \
+# --default-artifact-root mlruns \
+# --host 0.0.0.0 \
+# --port 5000
